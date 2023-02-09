604edc4488a80cbacdd9c6a96d7c1058314e67b4
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2023-02-09 07:27:18 +0200

    Fully delegate installation to rstantools

--- R/zzz.R
+++ R/zzz.R
@@ -3,9 +3,5 @@
     if (!("methods" %in% .packages()))
         attachNamespace("methods");
 
-    modules <- paste0("stan_fit4", names(stanmodels), "_mod");
-    for (m in modules) {
-        loadModule(m, what = TRUE);
-    }
 }
 
